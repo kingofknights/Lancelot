fefcc1a7e79408b13297f1b55a1e611b9f49318a
Lodhi, Vikram <vikram.lodhi@barco.com>
2023-06-18 12:44:17 +0530

    added main header

---
+++ include/Lancelot.hpp
@@ -0,0 +1,11 @@
+#ifndef LANCELOT_INCLUDE_LANCELOT_HPP
+#define LANCELOT_INCLUDE_LANCELOT_HPP
+
+#include "Lancelot/ContractInfo/ContractFetcher.hpp"
+#include "Lancelot/ContractInfo/ContractInfo.hpp"
+#include "Lancelot/ContractInfo/StoreProcedures.hpp"
+#include "Lancelot/Database/DatabaseHandler.hpp"
+#include "Lancelot/Logger/Logger.hpp"
+#include "Lancelot/Structure.hpp"
+
+#endif	// LANCELOT_INCLUDE_LANCELOT_HPP
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Lancelot/Database/DatabaseHandler.hpp"
+#include "Lancelot.hpp"
 
 int main() {
 	Lancelot::DatabaseHandler::GetInstance().InitConnection("tcp://localhost:3306", "root", "123456");
