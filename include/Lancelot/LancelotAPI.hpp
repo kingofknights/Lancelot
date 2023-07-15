59129a8039699481f56b42478ea5b7933d6437ed
kingofknights <vikram.smvdu@gmail.com>
2023-07-15 22:10:43 +0530

    moved API header to front

---
+++ include/Lancelot/LancelotAPI.hpp
@@ -0,0 +1,14 @@
+//
+// Created by kingofknights on 7/15/23.
+//
+
+#ifndef LANCELOT_INCLUDE_LANCELOT_LANCELOTAPI_HPP_
+#define LANCELOT_INCLUDE_LANCELOT_LANCELOTAPI_HPP_
+#pragma once
+
+#include "API/Adaptor/Adaptor.hpp"
+#include "API/Common/Common.hpp"
+#include "API/Common/StockPacket.hpp"
+#include "API/Strategy/Strategy.hpp"
+
+#endif	// LANCELOT_INCLUDE_LANCELOT_LANCELOTAPI_HPP_
