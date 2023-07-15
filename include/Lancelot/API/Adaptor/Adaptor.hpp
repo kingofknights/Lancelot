ada262b5d62a87f3405d6596bbe5174efd99c4ff
kingofknights <vikram.smvdu@gmail.com>
2023-07-15 17:15:34 +0530

    added function in Strategy and Adaptor

--- include/Lancelot/API/Adaptor/Adaptor.hpp
+++ include/Lancelot/API/Adaptor/Adaptor.hpp
@@ -5,9 +5,36 @@
 #ifndef LANCELOT_INCLUDE_LANCELOT_API_ADAPTOR_ADAPTOR_HPP_
 #define LANCELOT_INCLUDE_LANCELOT_API_ADAPTOR_ADAPTOR_HPP_
 
+#include <memory>
+#include <thread>
+#include <vector>
+
+using ThreadPointerT = std::unique_ptr<std::jthread>;
+using ThreadGroupT	 = std::vector<ThreadPointerT>;
+
 namespace Lancelot::API {
+	enum Exchange : int;
+	enum OrderRequest : int;
+	enum OrderStatus : int;
+
+	class StockPacket;
+	using StockPacketPtrT = std::shared_ptr<StockPacket>;
+
 	class Adaptor {
-	protected:
+	public:
+		virtual ~Adaptor() = default;
+
+		virtual void initialization(ThreadGroupT& threadGroup_) = 0;
+
+		virtual void forwardAssemble(const StockPacketPtrT& order_) = 0;
+
+		virtual bool execute(const StockPacketPtrT& order_, int price_, int quantity_, OrderRequest request_) = 0;
+
+		static void OrderResponse(const StockPacketPtrT& order_, OrderStatus status_);
+
+		static void OnDisconnection(Exchange exchange_);
+
+		static void OnConnection(Exchange exchange_);
 	};
 }  // namespace Lancelot::API
 #endif	// LANCELOT_INCLUDE_LANCELOT_API_ADAPTOR_ADAPTOR_HPP_
--- include/Lancelot/API/Strategy/Strategy.hpp
+++ include/Lancelot/API/Strategy/Strategy.hpp
@@ -4,10 +4,61 @@
 
 #ifndef LANCELOT_INCLUDE_LANCELOT_API_STRATEGY_STRATEGY_HPP_
 #define LANCELOT_INCLUDE_LANCELOT_API_STRATEGY_STRATEGY_HPP_
+#pragma once
 
-namespace Lancelot::API {
-	class Strategy {
-	protected:
-	};
-}  // namespace Lancelot::API
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+namespace Lancelot {
+	enum Side : int;
+
+	namespace API {
+
+		class Strategy;
+		class StockPacket;
+
+		using StrategyPtrT	  = std::shared_ptr<Strategy>;
+		using StockPacketPtrT = std::shared_ptr<StockPacket>;
+		using StrategyParamT  = std::unordered_map<std::string, std::string>;
+
+		class Strategy : public std::enable_shared_from_this<Strategy> {
+		public:
+			explicit Strategy(int strategy_);
+
+			virtual ~Strategy() = default;
+
+			virtual void paramEvent(const StrategyParamT& param_) = 0;
+
+			virtual void marketEvent(int token_) = 0;
+
+			virtual void orderEvent(int uniqueID_) = 0;
+
+			virtual void stopEvent() = 0;
+
+			[[nodiscard]] bool activated() const;
+
+			void setActivated(bool activated_);
+
+			template <class Child>
+			[[nodiscard]] static StrategyPtrT CreateInstance(int pf_, StrategyParamT strategyParameter_) {
+				return std::make_shared<Child>(pf_, strategyParameter_);
+			}
+
+		protected:
+			void destroy();
+
+			StockPacketPtrT getStockPacket(int token_, Side side_, const std::string& client_, const std::string& algo_, int ioc_, bool needEvent_ = false);
+
+		private:
+			void registerForData(int token_);
+
+			void registerSelf();
+
+		private:
+			bool _activated;
+			int	 _strategy;
+		};
+	}  // namespace API
+}  // namespace Lancelot
 #endif	// LANCELOT_INCLUDE_LANCELOT_API_STRATEGY_STRATEGY_HPP_
